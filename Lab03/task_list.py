d1bdd033da39d1eb0b735cc1e13e7321314a148a
Niulek456 <166750094+Niulek456@users.noreply.github.com>
2025-06-11 17:46:40 +0200

    Delete Lab03 directory

--- Lab03/task_list.py
+++
@@ -1,9 +0,0 @@
-class TaskList:
-    def __init__(self):
-        self._tasks = []
-
-    def add_task(self, task):
-        self._tasks.append(task)
-
-    def tasks(self):
-        return self._tasks
